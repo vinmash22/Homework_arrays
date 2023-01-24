d77caa25644f514ab7cff2ff993beddccdd6f88a
vinmash <vinmash@mail.ru>
2023-01-24 23:36:03 +0500

    task 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        System.out.println();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1, задача 2");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        System.out.println(Arrays.toString(array));
+        float[] array1 = new float[]{1.57f, 7.654f, 9.986f};
+        System.out.println(Arrays.toString(array1));
+        int[] array2 = {95, 87, 100, 110, 6, 7, 1250};
+        System.out.println(Arrays.toString(array2));
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        System.out.println(Arrays.toString(array));
+        float[] array1 = new float[]{1.57f, 7.654f, 9.986f};
+        System.out.println(Arrays.toString(array1));
+        int[] array2 = {95, 87, 100, 110, 6, 7, 1250};
+        System.out.println(Arrays.toString(array2));
     }
 }
